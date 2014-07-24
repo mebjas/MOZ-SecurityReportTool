5ac0764c3e41cd50ef237593e35797f214d17eb5
minhaz <minhazav@gmail.com>
2014-07-24 05:30:55 +0530

    Data structure added to store information

--- data/securityReport-new.js
+++ data/securityReport-new.js
@@ -15,11 +15,12 @@ var secConsole = {
 function downloadSecurityReport() {
 	var event = new CustomEvent("downloadReport-message");
 	document.documentElement.dispatchEvent(event);
-
 	// Logged to console from securityReportUI.js
 }
 
+/*
 function openSecurityReport() {
 	var event = new CustomEvent("openSecReport-msg");
 	document.documentElement.dispatchEvent(event);
 }
+*/
